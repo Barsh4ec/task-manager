cf7777913994099a009697e911ddbc10c3321938
Taras Borshch <tarasborshch4@gmail.com>
2023-10-06 19:48:34 +0300

    Fixed DB connection

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -66,9 +66,9 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-if not os.environ["DJANGO_DEBUG"]:
-    db_from_env = dj_database_url.config(conn_max_age=500)
-    DATABASES["default"].update(db_from_env)
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES["default"].update(db_from_env)
 
 
 AUTH_PASSWORD_VALIDATORS = [
